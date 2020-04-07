1df53d7e595ff4a67dc759c5d99ead2abb1fcbc0
Peter Moser <peterjamoser@bluewin.ch>
2020-04-07 12:24:18 +0200

    update 7.4.

--- Mobility_GroundTraffic.R
+++ Mobility_GroundTraffic.R
@@ -1,3 +1,7 @@
 source("Mobility_CarTraffic.R", encoding = "UTF-8")
 remove(list = ls())
 source("Mobility_PedestrianAndBicycleTraffic.R", encoding = "UTF-8")
+
+mob<-read.csv("Mobilität_AufkommenTerrestrischerVerkehr.csv", encoding="UTF-8")
+
+range(as.Date(mob$date))
